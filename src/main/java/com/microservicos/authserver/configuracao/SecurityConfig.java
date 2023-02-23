b4226dbcb1875a4810389981fe7c015207a72127
Matheus Cauã Rocha Porto <matheuscrp1@hotmail.com>
2023-02-23 11:46:49 -0300

    feat - Security Filter Chain

---
+++ src/main/java/com/microservicos/authserver/configuracao/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.microservicos.authserver.configuracao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain asSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
+
+        http.exceptionHandling(e -> e
+                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")));
+
+        
+        return http.build();
+
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain appSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .formLogin()
+                .and()
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+
+
+}
