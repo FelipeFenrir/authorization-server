d7654f9733793f7e365543bdb464876b507b53b3
Matheus Cauã Rocha Porto <matheuscrp1@hotmail.com>
2023-02-25 00:17:38 -0300

    feat - Client Entidade

--- src/main/java/com/microservicos/authserver/modelos/entidades/ClientServer.java
+++ src/main/java/com/microservicos/authserver/modelos/entidades/ClientServer.java
@@ -3,11 +3,21 @@ package com.microservicos.authserver.modelos.entidades;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Entity(name = "tb_client")
-public class ClientServer {
+public class ClientServer implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String clientId;
+    private String secret;
+    private String redirectUri;
+    private String scope;
+    private String authMethod;
+    private String grantType;
+
 }
